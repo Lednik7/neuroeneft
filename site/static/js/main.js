d0510749aa3c3ae7ecaac3e978ebceb5427b0f44
Zyryanova Maria <mariazyranova72@gmail.com>
2020-09-20 19:01:06 +0500

    календарь

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -1,1 +1,12 @@
-$('#datetimepicker').data("DateTimePicker").FUNCTION();
+window.onload = function() {
+    document.getElementById('click_3day').addEventListener('click', function (e) {
+        alert("1")
+    });
+    document.getElementById('click_week').addEventListener('click', function (e) {
+        alert("2")
+    });
+
+    document.getElementById('click_month').addEventListener('click', function (e) {
+        alert("3")
+    });
+};
